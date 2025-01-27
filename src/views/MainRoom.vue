710d1fea5cbad651e0b212fd36904f542788856d
chub <chub960@gmail.com>
2025-01-27 14:21:29 +0300

    join ws

---
+++ src/views/MainRoom.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  комната {{ $route.params.roomId }}
+</div>
+</template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+const $route = useRoute()
+</script>
+<style scoped>
+
+</style>
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,20 +2,29 @@
 <div>
   <button @click="createRoom">Новая комната</button>
   &nbsp;
-  <button>Присоединиться к существующей</button>
+  <input type="text" @input="joinRoomId = $event.target.value">
+  <button @click="joinRoom">Присоединиться к существующей</button>
 </div>
 </template>
 <script setup lang="ts">
 import {getUUID} from "@/helpers/helpers.ts";
 import {socket} from "@/socket.ts";
-
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const $router = useRouter()
+const joinRoomId = ref('')
 const createRoom = () => {
   const uid = getUUID();
   socket.emit('createRoom', uid);
   // socket.emit('events', { test: '42' })
 }
-socket.on('createRoom', (e) => {
-	console.log('createRoom', e, socket)
+const joinRoom = () => {
+  console.dir(joinRoomId.value)
+  socket.emit('joinRoom', joinRoomId.value);
+}
+socket.on('joinRoom', (e) => {
+	console.log('joinRoom', e, socket)
+  $router.push(`/${e.id}`)
 	// io.in("5991083c-67a3-4fff-abfd-b74efba33b3b").emit("eventsRoom", () => {
 	// 	console.log('Job')
 	// });
